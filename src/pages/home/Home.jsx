6067864b5ebbb4d9e3c9a065eac8f809d8a67b5a
Vaibhav <vaibhav.2071998@gmail.com>
2023-05-23 18:12:38 +0530

    added footer

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,6 +4,7 @@ import Navbar from "../../components/navbar/Navbar";
 import Header from "../../components/header/Header";
 import Featured from "../../components/featured/Featured";
 import FeaturedProperties from "../../components/featuredProperties/FeaturedProperties";
+import Footer from "../../components/footer/Footer";
 
 const Home = () => {
   return (
@@ -13,6 +14,7 @@ const Home = () => {
       <div className="homeContainer">
         <Featured />
         <FeaturedProperties />
+        <Footer />
       </div>
     </div>
   );
