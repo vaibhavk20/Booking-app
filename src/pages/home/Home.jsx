2b76f941644f88781abe433d7128c265931140eb
Vaibhav <vaibhav.2071998@gmail.com>
2023-05-23 15:20:57 +0530

    added home page features

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,19 @@
 import React from "react";
+import "./home.css";
 import Navbar from "../../components/navbar/Navbar";
 import Header from "../../components/header/Header";
+import Featured from "../../components/featured/Featured";
+import FeaturedProperties from "../../components/featuredProperties/FeaturedProperties";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
       <Header />
+      <div className="homeContainer">
+        <Featured />
+        <FeaturedProperties />
+      </div>
     </div>
   );
 };
